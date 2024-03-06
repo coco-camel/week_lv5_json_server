b35b94d6d535ec0aea7b7c4cd740da3a2ea3bd95
Juno <anint3647@gmail.com>
2024-03-07 01:44:59 +0900

    [Fix] : error 수정

--- src/apis/auth.js
+++ src/apis/auth.js
@@ -5,18 +5,18 @@ const authAxios = axios.create({
 });
 export default authAxios;
 
-export const getAuthAxios = (token, navigate) => {
-    const authAxios = axios.create({
-        baseURL: process.env.REACT_APP_SERVER_URL,
-        headers: {
-            Authorization: token,
-        }
-    }
-    );
-    authAxios.interceptors.response.use(res => res, async error => {
-        if (await error.response.status === 401) {
-            navigate("/")
-        }
-    })
-    return authAxios;
-}
+// export const getAuthAxios = (token, navigate) => {
+//     const authAxios = axios.create({
+//         baseURL: process.env.REACT_APP_SERVER_URL,
+//         headers: {
+//             Authorization: token,
+//         }
+//     }
+//     );
+//     authAxios.interceptors.response.use(res => res, async error => {
+//         if (await error.response.status === 401) {
+//             navigate("/")
+//         }
+//     })
+//     return authAxios;
+// }
--- src/apis/myPage.js
+++ src/apis/myPage.js
@@ -1,8 +1,8 @@
-import { getAuthAxios } from "./auth";
-import { getCookie } from "../cookies/cookies";
+// import { getAuthAxios } from "./auth";
+// import { getCookie } from "../cookies/cookies";
 
-export const getMyPage = async (navigate) => {
-    const accessToken = getCookie("accessToken");
-    const authAxios = getAuthAxios(accessToken, navigate);
-    await authAxios.get("/user").then(res => res.data).catch(error => error);
-};  
+// export const getMyPage = async (navigate) => {
+//     const accessToken = getCookie("accessToken");
+//     const authAxios = getAuthAxios(accessToken, navigate);
+//     await authAxios.get("/user").then(res => res.data).catch(error => error);
+// };  
